477797c358c03e9884917d2fa22e7fd08f42644e
cuspace <cuspace@gmail.com>
2014-03-01 03:05:28 +0900

    WebSocket client code is added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+var ws = new WebSocket('ws://192.168.25.53:8080');
+
 window.addEventListener("deviceorientation", function(e) {
   document.querySelector("#alpha").textContent = e.alpha;
   document.querySelector("#beta").textContent = e.beta;
   document.querySelector("#gamma").textContent = e.gamma;
+  ws.send('This is message from minfo websocket client.');
 }, true);
