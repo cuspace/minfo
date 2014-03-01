4f8c4a3dfb8ff669066a779bbd90869b41505d12
cuspace <cuspace@gmail.com>
2014-03-01 16:07:42 +0900

    Transformation 'device orientation' info to JSON string and send

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-var ws = new WebSocket('ws://192.168.25.53:8080');
+var ws = new WebSocket('ws://192.168.0.21:8080');
 
 window.addEventListener("deviceorientation", function(e) {
   document.querySelector("#alpha").textContent = e.alpha;
   document.querySelector("#beta").textContent = e.beta;
   document.querySelector("#gamma").textContent = e.gamma;
-  ws.send('This is message from minfo websocket client.');
+  var m = {
+      type: 'mc',
+      alpha: event.alpha,
+      beta: event.beta,
+      gamma: event.gamma,
+  };
+  ws.send(JSON.stringify(m));
 }, true);
